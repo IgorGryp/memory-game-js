4a6b4287c8ad9b350cf2813af92db08ca7223da1
Igor Gryp <igor.gryp@gmail.com>
2023-11-24 16:54:19 +0100

    added feature to flip cards

--- app.js
+++ app.js
@@ -19,13 +19,20 @@ const images = [
 const gameBoard = document.querySelector('.game-board');
 const resetBtn = document.querySelector('.reset-btn');
 
+/* Resets game */
 resetBtn.addEventListener('click', () => window.location.reload());
 
+/* Randomly rearranges the elements in the images array. */
 const randomImage = images.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
 for (let i = 0; i < images.length; i++) {
-  const cardBox = document.createElement('div');
-  cardBox.className = 'card-box';
-  cardBox.innerHTML = randomImage[i];
-  gameBoard.appendChild(cardBox);
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = randomImage[i];
+  gameBoard.appendChild(card);
+
+  /* Opens clicked card */
+  card.onclick = function () {
+    this.classList.add('card-open');
+  };
 }
