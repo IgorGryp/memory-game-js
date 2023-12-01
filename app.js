7b195897bf0e4f243730475c66c5de67defc6908
IgorGryp <igor.gryp@gmail.com>
2023-12-01 15:08:54 +0100

    fixed result check and error message

--- app.js
+++ app.js
@@ -35,8 +35,11 @@ const playerTwoName = document.querySelector('.player-2-name');
 const playerOneScore = document.querySelector('.player-1-score');
 const playerTwoScore = document.querySelector('.player-2-score');
 const player = document.querySelector('.player');
+const errorMessage = document.querySelector('.error-message');
 
-let currentPlayer = 1;
+let player1 = '';
+let player2 = '';
+let currentPlayer = '';
 let playerOneMatches = 0;
 let playerTwoMatches = 0;
 
@@ -52,28 +55,39 @@ function openModal() {
 
 /* --------------------------------------------------------- */
 
-startGameBtn.addEventListener('click', () => startGame());
+/* startGameBtn.addEventListener('click', () => {
+  player1 !== '' && player2 !== ''
+    ? startGame()
+    : (errorMessage.innerHTML = 'Please enter names for both players to start the game.');
+}); */
+
+startGameBtn.addEventListener('click', () => {
+  // Get player names from input fields
+  player1 = document.getElementById('player1').value;
+  player2 = document.getElementById('player2').value;
+  player1 !== '' && player2 !== ''
+    ? startGame()
+    : (errorMessage.innerHTML = 'Please enter names for both players to start the game.');
+});
 
 // Function to start the game
 function startGame() {
   playersSection.style.display = 'flex';
   gameSection.style.display = 'block';
-  // Get player names from input fields
-  let player1 = document.getElementById('player1').value;
-  let player2 = document.getElementById('player2').value;
+
+  // Display first players name
+  currentPlayer = player1;
+  player.innerHTML = `${currentPlayer}'s turn`;
 
   // Check if players entered names
   if (player1 && player2) {
-    // Display player names on the page
+    // Display player names and score on the page
     playerOneName.innerHTML = `Player 1: ${player1}`;
     playerOneScore.innerHTML = `Score: 0`;
     playerTwoName.innerHTML = `Player 2: ${player2}`;
     playerTwoScore.innerHTML = `Score: 0`;
     // Close the modal
     document.getElementById('modal').style.display = 'none';
-  } else {
-    // If any player didn't enter a name, show an alert
-    alert('Please enter names for both players to start the game.');
   }
 }
 
@@ -104,8 +118,8 @@ for (let i = 0; i < images.length; i++) {
             /* Add matching classes  */
             document.querySelectorAll('.card-open')[0].classList.add('card-match');
             document.querySelectorAll('.card-open')[1].classList.add('card-match');
-
-            if (currentPlayer === 1) {
+            /* Check which player is choosing the cards and add score */
+            if (currentPlayer === player1) {
               playerOneMatches++;
               playerOneScore.innerHTML = `Score: ${playerOneMatches}`;
             } else {
@@ -117,15 +131,16 @@ for (let i = 0; i < images.length; i++) {
               card.classList.remove('card-open');
             });
 
-            checkWin(); // Checks which player won
+            // Checks which player won
+            checkWin();
           } else {
             document.querySelectorAll('.card-open').forEach((card) => {
               card.classList.remove('card-open');
             });
 
             // Switch to the next player
-            currentPlayer = currentPlayer === 1 ? 2 : 1;
-            player.innerHTML = `Player ${currentPlayer}'s turn`;
+            currentPlayer = currentPlayer === player1 ? player2 : player1;
+            player.innerHTML = `${currentPlayer}'s turn`;
           }
         }
       }, 500);
@@ -133,11 +148,17 @@ for (let i = 0; i < images.length; i++) {
   };
 }
 
-/* Checks which player won */
+/* Checks which player won or if the game ended in a tie */
 function checkWin() {
   if (playerOneMatches + playerTwoMatches === (images.length - 1) / 2) {
     player.style.color = 'red';
-    player.innerHTML = `Player ${currentPlayer} won!`;
+    if (playerOneMatches > playerTwoMatches) {
+      player.innerHTML = `${player1} won!`;
+    } else if (playerOneMatches < playerTwoMatches) {
+      player.innerHTML = `${player2} won!`;
+    } else {
+      player.innerHTML = `Tie Game!`;
+    }
   }
 }
 
